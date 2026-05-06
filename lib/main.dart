7dca42c11f0d9ac4d0b4443a81983cef33db3800
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-05-06 17:12:00 -0300

    Atualização 06/05

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 183, 58, 58)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -35,17 +35,53 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Etec Adolpho Berezin - PAM1'),
+        title: const Text('AppBar'),
           backgroundColor: Colors.redAccent,
       ),
       body: const Center(
         child: Text(
-          'Desenvolvimento Mobile com Flutter',
+          'Body',
         style: TextStyle (
           fontSize: 12,
           color: Colors.blueGrey,
           ),
         ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const[
+            ListTile(
+              title: Text('End Drawer'),
+            )
+          ],
+        )
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            ListTile(
+              title: Text('Drawer'),
+            )
+          ], //children
+        )
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        // ações do botão
+        },
+        backgroundColor: const Color.fromARGB(255, 252, 96, 85),
+        child: Text('Botão Flutuante',
+          style: TextStyle(
+            fontSize: 12,
+            color: const Color.fromARGB(255, 122, 18, 10)
+              ),
+          ), 
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Botão 1'),
+        BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Botão 2'),
+        ],
       ),
     );
   }
