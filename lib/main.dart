b8df1690d9b0a2660fe10f9c6f3dbc63c72f3ca5
Davi_P <dpasinigemeos@gmail.com>
2026-06-17 17:13:02 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 183, 58, 58)),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 58, 81, 183)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -35,49 +35,59 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AppBar'),
-          backgroundColor: Colors.redAccent,
-      ),
-      body: const Center(
-        child: Text(
-          'Body',
-        style: TextStyle (
-          fontSize: 12,
-          color: Colors.blueGrey,
+        title: const Text('ETEC Mongaguá - PAM-1'),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+          elevation: 10,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
           ),
+      ),
+
+      endDrawer: const Drawer(
+        child: Center(child: Text('Meu EndDrawer (Configurações)')),
+      ),
+
+      drawer: const Drawer(
+        child: Center(child: Text('Meu Drawer (menu)')),
+      ),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.blueAccent,
+              child: CircleAvatar(
+                radius: 56,
+                backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/2021966489281306625/m0eMfmMZ.jpg'),
+              ),
+            ),
+
+            const SizedBox(height: 15,),
+            const Text(
+              'Davi Pasini Secundo',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold,),
+            ),
+              const Text(
+              '16 Anos - Mongaguá, SP',
+              style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+            ),
+            const Text(
+              'Vulgo: Bluy_P',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+            ),
+
+            //parei por aqui
+
+          ],
         ),
       ),
-      endDrawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const[
-            ListTile(
-              title: Text('End Drawer'),
-            )
-          ],
-        )
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const [
-            ListTile(
-              title: Text('Drawer'),
-            )
-          ], //children
-        )
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        // ações do botão
-        },
-        backgroundColor: const Color.fromARGB(255, 252, 96, 85),
-        child: Text('Botão Flutuante',
-          style: TextStyle(
-            fontSize: 12,
-            color: const Color.fromARGB(255, 255, 242, 241)            
-              ),
-          ), 
-      ),
+
       bottomNavigationBar: BottomNavigationBar(items: const[
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Botão 1'),
         BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Botão 2'),
